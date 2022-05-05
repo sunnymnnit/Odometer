b72d7a0f0bce620a3c164303928f988161b8bb8c
SUNNY <sunny9829681@gmail.com>
2022-05-05 15:21:55 +0530

    final touch

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ function Navbar()
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar scroll</a>
+    <a className="navbar-brand" href="#">Odometer</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
